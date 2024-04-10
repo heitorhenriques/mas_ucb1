d726eece5c4e19652da4f6a188c4e3f7681e2d27
HeitorHs <heiitorheenrique@gmail.com>
2024-04-10 14:45:17 -0300

    brute force implemented

--- src/agt/sample_agent.asl
+++ src/agt/sample_agent.asl
@@ -1,17 +1,43 @@
-// Agent sample_agent in project mas_ucb1
+// Agent sample_agent in project teste
 
 /* Initial beliefs and rules */
 
 /* Initial goals */
-
+action("0").
+action("1").
+action("2").
+action("3").
 !start.
 
 /* Plans */
 
 +!start : true
     <- .print("hello world.");
-       .date(Y,M,D); .time(H,Min,Sec,MilSec); // get current date & time
-       +started(Y,M,D,H,Min,Sec).             // add a new belief
+    .findall(S,action(S),L);
+    for (.member(K,L)){
+        .print("Testing action ",K);
+        !set_composition(K);
+        .wait(7500);
+        !get_avg;
+    };
+    !find_min(A,Ag);
+    //?min(A,Ag);
+    .send(client,tell,avg_time(Ag));
+    .print("The minimun avarege time was ",Ag," with action ",A);
+    .print("Setting the distributor with the smallest avarege time");
+    !set_composition(A);
+    .send(client,achieve,stop).
+
++!find_min(A,Ag) : composition(A,Ag) & not(composition(A1,Ag1) & Ag1 <= Ag & A \== A1)
+<- +min(A,Ag).
+
++!set_composition(S) : true
+<- send_operation(S).
+
++!get_avg : true
+    <- get_avg_time(Action,Avg_time);
+    +composition(Action,Avg_time);
+    .print("The avarege time is ",Avg_time).
 
 { include("$jacamo/templates/common-cartago.asl") }
 { include("$jacamo/templates/common-moise.asl") }
--- src/env/example/Counter.java
+++ src/env/example/Counter.java
@@ -1,8 +1,16 @@
-// CArtAgO artifact code for project mas_ucb1
+// CArtAgO artifact code for project teste
 
 package example;
 
 import cartago.*;
+import jason.stdlib.string;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Counter extends Artifact {
 	void init(int initialValue) {
@@ -22,6 +30,34 @@ public class Counter extends Artifact {
 		int newValue = prop.intValue()+inc;
 		prop.updateValue(newValue);
 		newValueArg.set(newValue);
+	}
+
+	@OPERATION
+	void get_avg_time(OpFeedbackParam<String> action, OpFeedbackParam<Double> avg_time) throws IOException, InterruptedException {
+		HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:3500/ucb/perception-data"))
+                .build();
+
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+		String[] parts = response.body().split(",");
+		String res1 = parts[0];
+		Double res2 = Double.parseDouble(parts[1]); 
+		avg_time.set(res2);
+		action.set(res1);
+	}
+
+	@OPERATION
+	void send_operation(String composition) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:3500/ucb/composition"))
+                .POST(HttpRequest.BodyPublishers.ofString(composition))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
 	}
 
 }
