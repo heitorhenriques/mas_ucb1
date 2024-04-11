d1e7ab61eb8c09843281230ed2e2dcee4d1c2eca
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2024-04-11 11:04:33 -0300

    GRAPH EXAMPLE

--- src/env/example/Counter.java
+++ src/env/example/Counter.java
@@ -20,34 +20,36 @@ public class Counter extends Artifact {
 
 	void init(int initialValue) {
 		defineObsProperty("count", initialValue);
+		LineChartExample.startGraph(null);
 	}
 
 	@OPERATION
 	void inc() {
 		ObsProperty prop = getObsProperty("count");
-		prop.updateValue(prop.intValue()+1);
+		prop.updateValue(prop.intValue() + 1);
 		signal("tick");
 	}
 
 	@OPERATION
 	void inc_get(int inc, OpFeedbackParam<Integer> newValueArg) {
 		ObsProperty prop = getObsProperty("count");
-		int newValue = prop.intValue()+inc;
+		int newValue = prop.intValue() + inc;
 		prop.updateValue(newValue);
 		newValueArg.set(newValue);
 	}
 
 	@OPERATION
-	void get_avg_time(OpFeedbackParam<String> action, OpFeedbackParam<Double> avg_time) throws IOException, InterruptedException {
+	void get_avg_time(OpFeedbackParam<String> action, OpFeedbackParam<Double> avg_time)
+			throws IOException, InterruptedException {
 		HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:3500/ucb/perception-data"))
-                .build();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("http://localhost:3500/ucb/perception-data"))
+				.build();
 
-        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 		String[] parts = response.body().split(",");
 		String res1 = parts[0];
-		Double res2 = Double.parseDouble(parts[1]); 
+		Double res2 = Double.parseDouble(parts[1]);
 		avg_time.set(res2);
 		action.set(res1);
 		avgTime.add(res2);
@@ -56,21 +58,21 @@ public class Counter extends Artifact {
 	@OPERATION
 	void send_operation(String composition) throws IOException, InterruptedException {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:3500/ucb/composition"))
-                .POST(HttpRequest.BodyPublishers.ofString(composition))
-                .build();
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("http://localhost:3500/ucb/composition"))
+				.POST(HttpRequest.BodyPublishers.ofString(composition))
+				.build();
 
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
+		HttpResponse<String> response = client.send(request,
+				HttpResponse.BodyHandlers.ofString());
 	}
 
 	@OPERATION
-	void confidence_level(double reward, double times_chosen, double iterations,OpFeedbackParam<Double> confidence_level){
+	void confidence_level(double reward, double times_chosen, double iterations,
+			OpFeedbackParam<Double> confidence_level) {
 		double result = (reward / times_chosen) + Math.sqrt((2 * Math.log(iterations)) / (1 + times_chosen));
 		confidence_level.set(result);
 	}
 
 }
-
